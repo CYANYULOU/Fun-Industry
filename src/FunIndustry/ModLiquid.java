1b950e0e9fc661cac8d9eb4dca9a1aa3f10dd2e2
rockxv <166192931+rockxv@users.noreply.github.com>
2025-08-27 16:22:47 +0800

    重载了一次

--- src/FunIndustry/ModLiquid.java
+++ src/FunIndustry/ModLiquid.java
@@ -8,7 +8,7 @@ public class ModLiquid {
     public static Liquid Fuel;
     public static void load() {
         CarbonDioxide = new Liquid("carbon-dioxide", Color.valueOf("ffffff"));
-        Fuel = new Liquid("fuel", Color.valueOf("997B3E")) {
+        Fuel = new Liquid("fuel", Color.valueOf("F5E6C8")) {
             {
                 flammability = 2.0f;
                 explosiveness = 2.0f;
@@ -16,4 +16,3 @@ public class ModLiquid {
         };
     }
 }
-
