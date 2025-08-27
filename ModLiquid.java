cfd98eed695eced34c5cb8eb6bb3b1c088447377
YanYLou <yey_2022@163.com>
2025-08-27 16:34:35 +0800

    reload fuel code and pic

---
+++ ModLiquid.java
@@ -0,0 +1,18 @@
+package FunIndustry;
+
+import arc.graphics.Color;
+import mindustry.type.Liquid;
+
+public class ModLiquid {
+    public static Liquid CarbonDioxide;
+    public static Liquid Fuel;
+    public static void load() {
+        CarbonDioxide = new Liquid("carbon-dioxide", Color.valueOf("ffffff"));
+        Fuel = new Liquid("fuel", Color.valueOf("E5DF9D")) {
+            {
+                flammability = 2.0f;
+                explosiveness = 2.0f;
+            }
+        };
+    }
+}
